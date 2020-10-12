53da8d3ef37f405942cac10eed6f93924bd5f017
hankdikeman <dikem003@umn.edu>
2020-10-11 21:15:31 -0500

    fixed sampling error

--- SampleGen.py
+++ SampleGen.py
@@ -12,7 +12,7 @@ def setSample(weatherData, n_samp, n_prior, col_target):
     # else only the amount of requested samples should be sent
     else:
         day_samp = np.random.choice(n_samp, size = n_days - nprior, replace = False)
-        x_samp = [day_samp,:]
+        x_samp = [day_samp:day_samp+3,:]
         y_samp = [day_samp+nprior,col_target]
     # return tuple of x and y values
     return (x_samp, y_samp)
