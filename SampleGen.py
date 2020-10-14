0544adc7004fe3f3c040040c8d96a02c1c36163b
hankdikeman <dikem003@umn.edu>
2020-10-13 23:14:32 -0500

    fixed matrix calls

--- SampleGen.py
+++ SampleGen.py
@@ -7,12 +7,12 @@ def setSample(weatherData, n_samp, n_prior, col_target):
     # if weatherData is equal to number of desired samples, return them all
     if(n_days <= n_samp-n_prior):
         day_samp = np.random.choice(n_days-n_prior, size = n_days - nprior, replace = False)
-        x_samp = [day_samp,:]
-        y_samp = [day_samp+nprior,col_target]
+        x_samp = weatherData[day_samp,:]
+        y_samp = weatherData[day_samp+nprior,col_target]
     # else only the amount of requested samples should be sent
     else:
         day_samp = np.random.choice(n_samp, size = n_days - nprior, replace = False)
-        x_samp = [day_samp:day_samp + 3,:]
-        y_samp = [day_samp+nprior,col_target]
+        x_samp = weatherData[day_samp:day_samp+3,:]
+        y_samp = weatherData[day_samp+nprior,col_target]
     # return tuple of x and y values
     return (x_samp, y_samp)
