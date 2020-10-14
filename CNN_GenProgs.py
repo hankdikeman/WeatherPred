b2cd442a3eb85567c58fb52f9b5ec177710442f1
hankdikeman <48107082+hankdikeman@users.noreply.github.com>
2020-10-14 15:11:20 -0500

    Convolutional neural net generation function

---
+++ CNN_GenProgs.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+# generates and returns convolutional model based on following parameters:
+# x_len = stations west to east, y_len = stations north to south
+# day_num = number of days modelled, num_fil = number of filters used
+def Gen_CNN_Model(x_len, y_len, day_num, num_fil):
+    # declare model
+    CNN_model = models.Sequential()
+    # 1 convolutional layer with num_fil 3x3 filters
+    CNN_model.add(layers.Conv2D(num_fil,
+                                (3,3),
+                                activation = 'relu',
+                                input_shape = (x_len, y_len, day_num)))
+    # 2x2 pooling layer
+    CNN_model.add(layers.MaxPooling2d((2, 2)))
+    # 2nd convolutional layer
+    CNN_model.add(layers.Conv2D(num_fil,
+                                (3,3),
+                                activation = 'relu'))
+    # 2nd 2x2 pooling layer
+    CNN_model.add(layers.MaxPooling2d((2, 2)))
+    # 3rd convolutional layer
+    CNN_model.add(layers.Conv2D(num_fil,
+                                (3,3),
+                                activation = 'relu'))
+    # 3rd pooling layer
+    CNN_model.add(layers.MaxPooling2d((2, 2)))
+    # flatten output to 1 dimension
+    CNN_model.add(layers.Flatten())
+    # 1st dense layer with 64 nodes
+    CNN_model.add(layers.Dense(64,
+                               activation = 'relu'))
+    # output layer with 1 output node (for temp)
+    CNN_model.add(layers.Dense(1))
+    # return generated model
+    return CNN_model
