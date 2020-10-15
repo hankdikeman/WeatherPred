9b58af2aab4adc1da8aa535449250b335721c216
hankdikeman <dikem003@umn.edu>
2020-10-14 21:19:38 -0500

    added plotting function to visualize accuracy

--- CNN_Driver_1.py
+++ CNN_Driver_1.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import numpy as np
 from CNN_GenProgs import *
+import matplotlib.pyplot as plt
 
 ##
 #   Need to include data processing and import function
@@ -19,3 +20,10 @@ CNN_model.compile(optimizer = 'adam',
 history = CNN_model.fit(train_x, train_y,
                         epochs = epoch_num,
                         validation_data=(test_x, test_y))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.show()
