e315f1777d621b6d3529908dd8509b8828de5f59
hankdikeman <dikem003@umn.edu>
2020-11-02 20:20:53 -0600

    fixed sample generation function for interped data

--- SampleGen.py
+++ SampleGen.py
@@ -1,18 +1,21 @@
 import numpy as np
 
-# Take in a matrix of weatherData in standard form and return n_samp samples
-def setSample(weatherData, n_samp, n_prior, col_target):
+# Take in a matrix of weatherData in standard form and return sample_size samples
+def setSample(weatherData, sample_size, days_before):
     # save number of sample days in weatherData
-    n_days = np.shape(weatherData)[0]
-    # if weatherData is equal to number of desired samples, return them all
-    if(n_days <= n_samp-n_prior):
-        day_samp = np.random.choice(n_days-n_prior, size = n_days - nprior, replace = False)
-        x_samp = weatherData[day_samp,:]
-        y_samp = weatherData[day_samp+nprior,col_target]
+    num_days = np.shape(weatherData)[0]
+    # if weatherData is equal or smaller to number of desired samples, return them all
+    if(num_days <= sample_size-days_before):
+        rand_days = np.random.choice(num_days-days_before, size = num_days - nprior, replace = False)
+        x_samp = weatherData[rand_days,:]
+        for day_added in range(start = 1, stop = nprior):
+            xsamp = vstack(xsamp, weatherData[rand_days+day_added,:])
+        y_samp = weatherData[rand_days+nprior,:]
     # else only the amount of requested samples should be sent
     else:
-        day_samp = np.random.choice(n_samp, size = n_days - nprior, replace = False)
-        x_samp = weatherData[day_samp:day_samp+3,:]
-        y_samp = weatherData[day_samp+nprior,col_target]
+        rand_days = np.random.choice(sample_size, size = num_days - nprior, replace = False)
+        for day_added in range(start = 1, stop = nprior):
+            xsamp = vstack(xsamp, weatherData[rand_days+day_added,:])
+        y_samp = weatherData[rand_days+nprior,:]
     # return tuple of x and y values
     return (x_samp, y_samp)
