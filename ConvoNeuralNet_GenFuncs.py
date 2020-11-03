ef575fe76e41cc1a245af4812b2d899902f42570
hankdikeman <dikem003@umn.edu>
2020-11-02 20:26:11 -0600

    modified convolutional NN gen function for new input format

--- ConvoNeuralNet_GenFuncs.py
+++ ConvoNeuralNet_GenFuncs.py
@@ -1,18 +1,18 @@
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import layers, models
 import numpy as np
 
 # generates and returns convolutional model based on following parameters:
-# x_len = stations west to east, y_len = stations north to south
+# long_nodes = node num west to east, lat_nodes = node num north to south
 # day_num = number of days modelled, num_fil = number of filters used
-def Gen_CNN_Basic(x_len, y_len, day_num, num_fil):
+def Gen_CNN_Basic(long_nodes, lat_nodes, day_num, num_fil):
     # declare model
     CNN_model = models.Sequential()
     # 1 convolutional layer with num_fil 3x3 filters
     CNN_model.add(layers.Conv2D(num_fil,
                                 (3,3),
                                 activation = 'relu',
-                                input_shape = (x_len, y_len, day_num)))
+                                input_shape = (long_nodes, lat_nodes, day_num)))
     # 2x2 pooling layer
     CNN_model.add(layers.MaxPooling2d((2, 2)))
     # 2nd convolutional layer
@@ -32,7 +32,7 @@ def Gen_CNN_Basic(x_len, y_len, day_num, num_fil):
     # 1st dense layer with 64 nodes
     CNN_model.add(layers.Dense(64,
                                activation = 'relu'))
-    # output layer with 1 output node (for temp)
-    CNN_model.add(layers.Dense(1))
+    # output layer with map for long and lat
+    CNN_model.add(layers.Dense(long_nodes*lat_nodes))
     # return generated model
     return CNN_model
