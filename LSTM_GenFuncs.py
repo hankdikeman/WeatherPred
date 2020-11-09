ba45c9041ea9eb6fb64ee944ee2c5d16a6876ce9
hankdikeman <48107082+hankdikeman@users.noreply.github.com>
2020-11-09 09:29:29 -0600

    added short LSTM network

--- LSTM_GenFuncs.py
+++ LSTM_GenFuncs.py
@@ -24,7 +24,25 @@ def Gen_LSTM_Basic(long_nodes, lat_nodes, day_num):
     LSTM_model.compile(optimizer='adam', loss='mean_squared_error',metrics=['accuracy'])
 
     return LSTM_model
-####
-#   LSTM number 2
-#   insert model generation function here
-####
+
+# LSTM model 2 with different loss and shorter layer seq
+def short_LSTM(long_nodes, lat_nodes, day_num):
+    # declare model
+    LSTM_short = models.Sequential()
+    # 2 LSTM layers
+    LSTM_short.add(layers.LSTM(512, return_sequences=True,
+                        input_shape=(day_num, long_nodes*lat_nodes),
+                        dropout=0.2, recurrent_dropout=0.2
+                        ))
+    LSTM_short.add(layers.LSTM(512,
+                         return_sequences=False,
+                         dropout=0.2
+                        ))
+    # add dense processing layer
+    LSTM_short.add(layers.Dense(256, activation = 'tanh'))
+    # add dense output layer
+    LSTM_short.add(layers.Dense(long_nodes*lat_nodes, activation = 'sigmoid'))
+    # compile LSTM model
+    LSTM_short.compile(loss=keras.losses.kullback_leibler_divergence, optimizer="adam", metrics=['accuracy'])
+    # return model
+    return LSTM_short
