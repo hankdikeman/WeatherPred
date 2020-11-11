e74f83f1544676c9884a07ec8c1713fcb5dd3c65
hankdikeman <dikem003@umn.edu>
2020-11-11 01:19:40 -0600

    finished model construction for CNN func

--- ConvoNeuralNet_GenFuncs.py
+++ ConvoNeuralNet_GenFuncs.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, models, Input
 import numpy as np
 
 # generates and returns convolutional model based on following parameters:
@@ -41,8 +41,76 @@ def Gen_CNN_Basic(long_nodes, lat_nodes, day_num, num_fil):
 #   CNN number 2: batch normalization
 #   insert model generation function here
 ####
+def Gen_CNN_BN(long_nodes, lat_nodes, day_num, num_fil):
+    # declare model
+    CNN_model = models.Sequential()
+    # 1 convolutional layer with num_fil 3x3 filters
+    CNN_model.add(layers.Conv2D(num_fil,
+                                (3,3),
+                                input_shape = (long_nodes, lat_nodes, day_num)))
+    # batch norm layer
+    CNN_model.add(BatchNormalization())
+    # activation layer
+    model.add(layers.Activation("relu"))
+    # 2x2 pooling layer
+    CNN_model.add(layers.MaxPooling2d((2, 2)))
+    # 2nd convolutional layer
+    CNN_model.add(layers.Conv2D(num_fil,
+                                (3,3)))
+    # batch norm layer
+    CNN_model.add(BatchNormalization())
+    # activation layer
+    model.add(layers.Activation("relu"))
+    # 2nd 2x2 pooling layer
+    CNN_model.add(layers.MaxPooling2d((2, 2)))
+    # 3rd convolutional layer
+    CNN_model.add(layers.Conv2D(num_fil,
+                                (3,3)))
+    # batch norm layer
+    CNN_model.add(BatchNormalization())
+    # activation layer
+    model.add(layers.Activation("relu"))
+    # 3rd pooling layer
+    CNN_model.add(layers.MaxPooling2d((2, 2)))
+    # flatten output to 1 dimension
+    CNN_model.add(layers.Flatten())
+    # 1st dense layer with 64 nodes
+    CNN_model.add(layers.Dense(64,
+                               activation = 'relu'))
+    # output layer with map for long and lat
+    CNN_model.add(layers.Dense(long_nodes*lat_nodes, activation = 'sigmoid'))
+    # return generated model
+    return CNN_model
 
 ####
 #   CNN number 3: CNN-LSTM Neural Net
 #   insert model generation function here
 ####
+# shape (n_frames, width, height, channels)
+def Gen_CNN_LSTM(long_nodes, lat_nodes, day_num, num_fil):
+    # declare model
+    CNN_model = models.Sequential()
+    # add input layer
+    Input(shape=(None, long_nodes, lat_nodes, 1))
+    # create CNN-LSTM layer w batch norm
+    CNN_model.add(layers.ConvLSTM2D(num_fil, dropout=0.3, recurrent_dropout=0.3))
+    CNN_model.add(BatchNormalization())
+    # create CNN-LSTM layer w batch norm
+    CNN_model.add(layers.ConvLSTM2D(num_fil, dropout=0.3, recurrent_dropout=0.3))
+    CNN_model.add(BatchNormalization())
+    # create CNN-LSTM layer w batch norm
+    CNN_model.add(layers.ConvLSTM2D(num_fil, dropout=0.3, recurrent_dropout=0.3))
+    CNN_model.add(BatchNormalization())
+    # create CNN-LSTM layer w batch norm
+    CNN_model.add(layers.ConvLSTM2D(num_fil, dropout=0.3, recurrent_dropout=0.3))
+    CNN_model.add(BatchNormalization())
+    # create CNN-LSTM layer w batch norm
+    CNN_model.add(layers.ConvLSTM2D(num_fil, dropout=0.3, recurrent_dropout=0.3))
+    CNN_model.add(BatchNormalization())
+    # two dense layers
+    CNN_model.add(layers.Dense(64,
+                               activation = 'relu'))
+    CNN_model.add(layers.Dense(64,
+                               activation = 'relu'))
+    # output layer with map for long and lat
+    CNN_model.add(layers.Dense(long_nodes*lat_nodes, activation = 'sigmoid'))
