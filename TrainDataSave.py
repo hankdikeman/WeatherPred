0166d8835c48a3ae3052a9cfccf777acf8026d77
Patrickgibbs <gibbo143@umn.edu>
2020-11-24 18:02:36 -0600

    COmmit

--- TrainDataSave.py
+++ TrainDataSave.py
@@ -1,23 +1,23 @@
 import numpy as np
 import pandas as pd
 import datetime
-from MNWeatherPull import *
+from TOBS_US_weather_pull import *
 
 # set start and end date of weather pull
-startDate = datetime.date(2018, 12, 21)
+startDate = datetime.date(2000, 12, 21)
 day_jump = datetime.timedelta(days = 1)
 n_days = 730
-csvname = "MNTrainData"
+csvname = "USTrainData"
 
 for timejump in range(n_days):
     # set date range
     dateRange = (startDate, startDate)
     # pull data and assign to numpy array
-    trainData = PullMNWeather(dateRange)
+    trainData = TOBS_US_weather_pull(dateRange)
     # save to csv file
-    with open('../../Desktop/'+csvname+'.csv', 'ab') as f:
-        np.savetxt(f, trainData, fmt = '%d', newline = ", ", delimiter = ',')
-        f.write(b"\n")
+    with open('~/Desktop/WeatherData/'+csvname+'.csv', 'ab') as f:
+        np.savetxt(f, trainData, fmt = '%d', newline = ',', delimiter = ',')
+    
 
     print("saved to file with name " + csvname)
     # add day to startDate
