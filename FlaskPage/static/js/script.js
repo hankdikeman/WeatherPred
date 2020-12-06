797938fc41f58382b46dc0d9c15ba66ab3fbc192
Shri Venkat <venka146@umn.edu>
2020-12-05 22:22:38 -0600

    Made changes to about page to include images and then text underneath once image is clicked. Made an images folder in FlaskPage/static.

--- FlaskPage/static/js/script.js
+++ FlaskPage/static/js/script.js
@@ -53,3 +53,15 @@ function imageZoom(imgID, resultID) {
     return {x : x, y : y};
   }
 }
+
+$(document).ready(function() {
+
+
+$('[data-toggle="collapse"]').click(function() {
+  $(this).toggleClass( "active" );
+  if ($(this).hasClass("active")) {
+    $(this).text("Hide");
+  } else {
+    $(this).text("Show");
+  }
+});
