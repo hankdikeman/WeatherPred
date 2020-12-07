064c88ecd51d0040f96a55e3d8b1f10ead633609
Shri Venkat <venka146@umn.edu>
2020-12-06 19:55:18 -0600

    Started some queries to load csv files into database using SQLAlchemy.

--- FlaskPage/app.py
+++ FlaskPage/app.py
@@ -6,6 +6,54 @@ import os
 
 # declare app
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+db = SQLAlchemy(app)
+
+# Check if there is a header in csv file.
+def Load_Data(file_name):
+    data = genfromtxt(file_name, delimiter=',', skip_header=0, converters={0: lambda s: str(s)})
+    return data.tolist()
+
+Base = declarative_base()
+
+class Weather_History(Base):
+    #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
+    __tablename__ = 'Weather_History'
+    __table_args__ = {'sqlite_autoincrement': True}
+    #tell SQLAlchemy the name of column and its attributes:
+    id = Column(Integer, primary_key=True, nullable=False)
+    date = Column(Date)
+    temp = Column(Float)
+
+if __name__ == "__main__":
+    t = time()
+
+    #Create the database
+    engine = create_engine('sqlite:///weather.db')
+    Base.metadata.create_all(engine)
+
+    #Create the session
+    session = sessionmaker()
+    session.configure(bind=engine)
+    s = session()
+
+    try:
+        file_name = "t.csv" #sample CSV file used:
+        data = Load_Data(file_name)
+
+        for i in data:
+            record = Weather_History(**{
+                'date' : datetime.strptime(i[0], '%d-%b-%y').date(),
+                'temp : i[1]
+            })
+            s.add(record) #Add all the records
+
+        s.commit() #Attempt to commit all the records
+    except:
+        s.rollback() #Rollback the changes on error
+    finally:
+        s.close() #Close the connection
+
 
 # define route to static images folder
 PHOTO_FOLDER = 'testpics'
