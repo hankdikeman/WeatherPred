769b5d50d577c7ab1a9fd3094e45c2c9152142e7
Shri Venkat <venka146@umn.edu>
2020-12-13 15:50:10 -0600

    Took out conflicting code. Made separate js file for about page

---
+++ FlaskPage/static/js/aboutPage.js
@@ -0,0 +1,8 @@
+$('[data-toggle="collapse"]').click(function() {
+  $(this).toggleClass( "active" );
+  if ($(this).hasClass("active")) {
+    $(this).text("Hide");
+  } else {
+    $(this).text("Show");
+  }
+});
--- FlaskPage/static/js/script.js
+++ FlaskPage/static/js/script.js
@@ -56,12 +56,4 @@ function imageZoom(imgID, resultID) {
 
 $(document).ready(function() {
 
-
-$('[data-toggle="collapse"]').click(function() {
-  $(this).toggleClass( "active" );
-  if ($(this).hasClass("active")) {
-    $(this).text("Hide");
-  } else {
-    $(this).text("Show");
-  }
-});
+}
