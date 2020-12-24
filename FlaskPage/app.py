80aae7f69dfe944aefc64d1c62203ef14719d76a
hankdikeman <dikem003@umn.edu>
2020-12-24 10:36:23 -0600

    removed debugging code accidentally left in

--- FlaskPage/app.py
+++ FlaskPage/app.py
@@ -1,7 +1,7 @@
 # import Flask and SQLAlchemy
 import matplotlib as mpl
 from matplotlib.figure import Figure
-# mpl.use('Agg')
+mpl.use('Agg')
 from flask import Flask, render_template, request, redirect, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
@@ -40,36 +40,20 @@ def display_format(data_line):
             lat[count] = lats
             temps[count] = (data_line[count])
             count += 1
-    print(temps[1200:1230])
     return long,lat,temps
 
 def gen_folium_map(longitude, latitude, data_line):
-    print("2nd long shape " + str(np.shape(longitude)))
-    print("2nd lat shape " + str(np.shape(latitude)))
-    print("2nd temp shape " + str(np.shape(data_line)))
-    print(longitude[1200:1230])
-    print(latitude[1200:1230])
-    print(data_line[1200:1230])
-    print(np.sum(data_line))
-    for dp in data_line:
-        print(dp)
     # make meshes
     longmesh,latmesh = np.meshgrid(lat_vals, lon_vals)
     # make initial folium map
     folium_map = folium.Map(location=start_coords, zoom_start = 4, height = '75%')
     # make temperature mesh more dense
-    print("error1")
     temp_mesh = griddata((longitude, latitude), data_line, (longmesh, latmesh), method = 'linear')
-    print(np.shape(temp_mesh))
-    print(np.shape(longmesh))
-    print(np.shape(latmesh))
     # generate matplotlib contour plot
-    print("error2")
     fig = Figure()
     ax = fig.add_subplot(111)
     temp_contour = ax.contourf(longmesh, latmesh, temp_mesh, alpha = 0.5, linestyles = 'None', vmin = 0, vmax = 100)
     # generate geojson list
-    print("error3")
     temp_geojson = geojsoncontour.contourf_to_geojson(
                         contourf = temp_contour,
                         min_angle_deg = 3.0,
@@ -78,7 +62,6 @@ def gen_folium_map(longitude, latitude, data_line):
                         fill_opacity = 0.5
                         )
     # make folium layer
-    print('error4')
     folium.GeoJson(
         temp_geojson,
         style_function=lambda x: {
@@ -123,11 +106,10 @@ def browse(day):
     ##
     # pull one line from csv
     # three lists from csv line for lat, long, and temp
+    data_line= np.genfromtxt('USTrainData1_1_2002TO9_17_2004.csv', delimiter=',')[100,:-4]
+    data_line = np.reshape(data_line, newshape = (17500))
+    # couple with latitude and longitude data
     long_data,lat_data,temp_data = display_format(data_line)
-
-    print("1st long shape " + str(np.shape(long_data)))
-    print("1st lat shape " + str(np.shape(lat_data)))
-    print("1st temp shape " + str(np.shape(temp_data)))
 
     # generate folium map from function
     folium_map = gen_folium_map(longitude = long_data, latitude = lat_data, data_line = temp_data)
