fbb7f72cf4fb8e9cc073af5fc37e6b6a3e0a2ab0
hankdikeman <dikem003@umn.edu>
2020-12-25 16:33:20 -0600

    mask for weather station data, function to generate mask

--- find_null_loc.py
+++ find_null_loc.py
@@ -10,11 +10,11 @@ for row in range(np.shape(csv_data)[0]):
     data_line = np.reshape(csv_data[row,:], newshape = (17500))
     for col in range(np.shape(data_line)[0]):
         if (data_line[col] != 0 and null_data[col] == 1):
-            null_data[col] = np.NaN
+            null_data[col] = 0
             print("boing " + str(count))
             count = count+1
 
 null_data = null_data.reshape(100,175)
 
-plt.imshow(null_data, cmap = 'gray')
-plt.show()
+with open('FlaskPage/static/no_stations_mask.npy', 'wb') as filename:
+    np.save(filename, null_data)
