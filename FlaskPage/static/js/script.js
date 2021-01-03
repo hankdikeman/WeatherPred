05acb71094fdbd07d66055f212ed4901e0140e8b
Shri Venkat <venka146@umn.edu>
2021-01-02 18:26:39 -0600

    Made a jumbotron for about me and changed background color.

--- FlaskPage/static/js/script.js
+++ FlaskPage/static/js/script.js
@@ -1,13 +1,1 @@
 
-/* $(document).ready(function() {
-
-
-$('[data-toggle="collapse"]').click(function() {
-  $(this).toggleClass( "active" );
-  if ($(this).hasClass("active")) {
-    $(this).text("Hide");
-  } else {
-    $(this).text("Show");
-  }
-});
-*/
