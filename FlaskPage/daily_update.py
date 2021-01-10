85b7077e808696d215f1e0008583cbc1da8115ed
hankdikeman <dikem003@umn.edu>
2021-01-10 14:45:14 -0600

    additional framework for update function

--- FlaskPage/daily_update.py
+++ FlaskPage/daily_update.py
@@ -1,12 +1,48 @@
 import json
 import numpy as np
 from datetime import datetime
-from app import db, WeatherDay
+from app import db, WeatherDay, pull_db_instance, unpack_db_entry
+from tensorflow.keras import models
 
-# delete old predictions more than one day out and commit
 
-# delete oldest actual data day and commit
+# placeholder function for deleting predicted/actual day
+def delete_day(target_date, predictive):
+    WeatherDay.query.filter(
+        WeatherDay.date == target_date,
+        WeatherDay.predictive == predictive
+    ).delete()
+    db.session.commit()
 
-# pull new day of data and commit
 
-# predict next ten days and commit
+# function to load and return neural network for weather prediction
+def load_weather_model(filename):
+    # load weather model from file
+    weather_model = models.load_model(filename)
+    # return loaded model
+    return weather_model
+
+
+##
+# This is a function to update the database for weather prediction data
+##
+if __name__ == "__main__":
+    # store current date
+    curr_date = datetime.now()
+    back_limit = curr_date - datetime.timedelta(days=30)
+    front_limit = curr_date + datetime.timedelta(days=10)
+
+    # initialize weather prediction table
+    db.create_all()
+
+    # delete old predictions more than one day out and commit
+    del_date = datetime.now()
+    while del_date <= front_limit:
+        delete_day(target_date=del_date, predictive=True)
+        del_date = del_date + datetime.timedelta(days=1)
+
+    # delete oldest actual data day and commit
+    delete_day(target_date=back_limit, predictive=False)
+
+    # pull new day of data and commit
+
+    # predict next ten days and commit
