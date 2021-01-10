2e0179041b68fdfb513639a09b4e3d89eca10166
hankdikeman <dikem003@umn.edu>
2021-01-10 12:42:31 -0600

    structure of daily update function

---
+++ FlaskPage/daily_update.py
@@ -0,0 +1,12 @@
+import json
+import numpy as np
+from datetime import datetime
+from app import db, WeatherDay
+
+# delete old predictions more than one day out and commit
+
+# delete oldest actual data day and commit
+
+# pull new day of data and commit
+
+# predict next ten days and commit
