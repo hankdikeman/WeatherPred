6a1e6c5aa3ce3a9719a7ffb361da7decd04478be
Shri Venkat <venka146@umn.edu>
2021-01-14 23:18:30 -0600

    Fixed error, replaced min and max

--- TrainDataSave.py
+++ TrainDataSave.py
@@ -5,12 +5,8 @@ from TOBS_US_weather_pull import *
 
 # set start and end date of weather pull
 # Started on 2002/01/01 format of date(year, month, day) no need for 09, just 9
-<<<<<<< HEAD
-startDate = datetime.date(2008, 1, 21 )  # start V3 on 2002, 11, 15 to 2004, 9, 16, V2 good from 2002, 1, 1 to 2002, 11, 14
-=======
 # start V3 on 2002, 11, 15 to 2004, 9, 16, V2 good from 2002, 1, 1 to 2002, 11, 14
-startDate = datetime.date(2008, 1, 6)
->>>>>>> 7f854b6bb33fefd82c24b68e8d424842a730c917
+startDate = datetime.date(2000, 1, 1)
 # Start V4 on 2004, 9, 16 to 2005, 3, 16 to 2005, 4, 1. Start V5 on 2005, 4, 2 to 2005, 4, 16
 # Start V6 on 2005, 4, 17 to 2005, 5, 31. Start V7 on 2005, 6, 1 to 2005,11, 13
 # Start V8 on 2005, 11, 14 to 2005, 12, 28. Start V9 on 2005, 12, 29 to 2006, 6, 12
@@ -21,7 +17,7 @@ startDate = datetime.date(2008, 1, 6)
 
 day_jump = datetime.timedelta(days=1)
 n_days = 720
-csvname = "USTrainDataCurrent(11, 22, 2020 to )"
+csvname = "TrainData01012000"
 
 for timejump in range(n_days):
     # pull data and assign to numpy array
@@ -32,7 +28,7 @@ for timejump in range(n_days):
     maxtemp = np.amax(trainData)
     mintemp = np.amin(trainData)
     for i in range(50):
-        print("min: " + str(maxtemp) + "  max: " + str(mintemp))
+        print("max: " + str(maxtemp) + "  min: " + str(mintemp))
     # save to csv file
     with open('/Users/uvman/OneDrive/Desktop/WeatherData/' + csvname + '.csv', 'a+b') as f:
         np.savetxt(f, trainData, fmt='%d', newline=',', delimiter=',')
