47123b2c1b472fe4f2deaab59102fbe42d111989
hankdikeman <dikem003@umn.edu>
2021-01-20 09:08:20 -0600

    updates to navigation page and updates to data pulling function

--- TrainDataSave.py
+++ TrainDataSave.py
@@ -2,28 +2,33 @@ import numpy as np
 import pandas as pd
 import datetime
 from TOBS_US_weather_pull import *
+import os
 
 # set start and end date of weather pull
 # Started on 2000/01/01 format of date(year, month, day) no need for 09, just 9
 # start V1 on 2000, 1, 1. Start V2 2000, 7, 30
-startDate = datetime.date(2000, 7, 30)
+# start V2 on 2000, 7, 30 Start V3 2001, 2, 25
+# start V3 on 2001, 2, 25 Start V4 2001, 9, 18
+# start V4 on 2001, 9, 18 Start V5 2002, 4, 15
+startDate = datetime.date(2002, 4, 15)
 
 day_jump = datetime.timedelta(days=1)
 n_days = 720
-csvname = "TrainData01012000"
+csvname = os.path.join(os.getcwd(), 'TrainData04152002')
 
 for timejump in range(n_days):
+    print("start day: " + str(timejump))
     # pull data and assign to numpy array
     trainData = TOBS_US_weather_pull(startDate)
+    maxtemp = np.amax(trainData)
+    mintemp = np.amin(trainData)
     date = np.array([startDate.year, startDate.month,
                      startDate.day]).reshape((1, 3))
     trainData = np.append(trainData, date, axis=1)
-    maxtemp = np.amax(trainData)
-    mintemp = np.amin(trainData)
     for i in range(50):
         print("max: " + str(maxtemp) + "  min: " + str(mintemp))
     # save to csv file
-    with open('/Users/uvman/OneDrive/Desktop/WeatherData/' + csvname + '.csv', 'a+b') as f:
+    with open(csvname + '.csv', 'a+b') as f:
         np.savetxt(f, trainData, fmt='%d', newline=',', delimiter=',')
         f.write(b"\n")
 
