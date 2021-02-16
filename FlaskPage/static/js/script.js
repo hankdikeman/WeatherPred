e726dda5b9b044d7d9697f098e9506d47bdc8817
Shri Venkat <venka146@umn.edu>
2021-02-15 21:45:04 -0600

    Carosel with pictures, missing Henry's picture and corresponding final adjustments.

--- FlaskPage/static/js/script.js
+++ FlaskPage/static/js/script.js
@@ -62,7 +62,6 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  console.log(slides.length);
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
